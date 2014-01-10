90fb828cee9d08dee81f21cdf0fc687d98873705
wibisono <a.wibisono@uva.nl>
2014-01-10 16:37:11 +0100

    Count triples example

--- src/test/resources/python/ducktape.py
+++ src/test/resources/python/ducktape.py
@@ -6,66 +6,64 @@ import functools
 
 module_registry = {}
 
-class DucktapeModule(object):
-	def __init__(self, name):
-		self.name = name;
-
-
 def main(*types): 
 
     def decorator(original_function):
 
-	# What I wanted here is that this function 
-	fspec = inspect.getargspec(original_function)
+        # What I wanted here is that this function 
+        fspec = inspect.getargspec(original_function)
 
-	# input names are actually arg names
-	inputnames = fspec.args
+        # input names are actually arg names
+        inputnames = fspec.args
 
-	moduleName = original_function.__name__
+        moduleName = original_function.__name__
 
-	module_registry[moduleName] = {} 
+        module_registry[moduleName] = {} 
 
-	module_registry[moduleName]["name"] = moduleName 
-	module_registry[moduleName]["description"] = original_function.__doc__ 
+        module_registry[moduleName]["name"] = moduleName 
+        module_registry[moduleName]["description"] = original_function.__doc__ 
 
-	module_registry[moduleName]["inputs"] = inputnames
-	module_registry[moduleName]["input_types"] = [t for t in types[:-1]]
+        module_registry[moduleName]["inputs"] = inputnames
+        module_registry[moduleName]["input_types"] = [t for t in types[:-1]]
 
-	module_registry[moduleName]["outputs"] = [moduleName]
-	module_registry[moduleName]["output_types"] = [o for o in types[-1:]]
+        module_registry[moduleName]["outputs"] = [moduleName]
+        module_registry[moduleName]["output_types"] = [o for o in types[-1:]]
 
-
-
-
-	@functools.wraps(original_function)
+        @functools.wraps(original_function)
         def wrapper(*args):
-            # types include output
-            assert len(types) == len(args)+1
 
-	    #try to open file for each argnames and assigned it to args.
-	    #if these inputs somehow are not available, then return original function
-	    try :
-		newargs = [];
-		for f_input in inputnames :
-			fi = open (f_input)
-			value = msgpack.unpack(fi.read())
-			newargs.append( value )
+            #try to open file for each argnames and assigned it to args.
+            #if these inputs somehow are not available, then return original function
+            try :
+                newargs = [];
+                for f_input in inputnames :
+                        fi = open (f_input)
+                        value = msgpack.unpackb(fi.read())
+                        newargs.append( value )
 
+                # Call the function using this.           
+                output = original_function(*newargs)
 
-		# Call the function using this.           
-		output = original_function(*newargs)
+                fo = open(original_function.__name__, "w")
+                fo.write(msgpack.packb(output))
+                fo.close()
 
-		fo = open(original_function.__name__, "w")
-		fo.write(msgpack.pack(output))
-		fo.close()
+                
+                return output
 
-		
-		return output
-
-	    except:
-		return original_function(*args)
+            except:
+                return original_function(*args)
 
         return wrapper
 
     return decorator 
 
+def dump_registry () :
+    """ Dumping module registry to be used in ducktape python domain to get information about module"""
+    f=open('moduleInfo.msg','w')
+    for module in module_registry:
+        cur = module_registry[module]
+        # This list is how msgpack expect MessageInfo object to be passed to java.
+        infoaslist = [cur['name'], cur['description'], cur['inputs'], cur['input_types'], cur['outputs'], cur['output_types']]
+        f.write(msgpack.packb(infoaslist))
+    f.close()
