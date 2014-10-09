1bd808200af955fc48e1cd376ef5b27f373ab625
wibisono <a.wibisono@uva.nl>
2014-10-09 10:08:11 +0200

    Sending the whole workflow to one of the task instead of individual modules. Latter approach would not maintain the relations between input/outputs of dependent modules

--- src/main/java/org/data2semantics/platform/execution/ClusterExecutionProfile.java
+++ src/main/java/org/data2semantics/platform/execution/ClusterExecutionProfile.java
@@ -7,17 +7,29 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ServiceLoader;
 
+import org.data2semantics.platform.Global;
+import org.data2semantics.platform.core.Branch;
 import org.data2semantics.platform.core.Module;
 import org.data2semantics.platform.core.ModuleInstance;
+import org.data2semantics.platform.core.State;
+import org.data2semantics.platform.core.Workflow;
+import org.data2semantics.platform.core.data.Input;
+import org.data2semantics.platform.core.data.InstanceInput;
+import org.data2semantics.platform.core.data.InstanceOutput;
+import org.data2semantics.platform.core.data.ReferenceInput;
 import org.data2semantics.platform.exception.ClusterConfigurationException;
 import org.data2semantics.platform.reporting.Reporter;
+import org.openrdf.rio.RDFParserFactory;
 
 import edu.rit.io.InStream;
 import edu.rit.io.OutStream;
 import edu.rit.pj2.Job;
 import edu.rit.pj2.Task;
+import edu.rit.pj2.TaskSpec;
 import edu.rit.pj2.Tuple;
+import edu.rit.pj2.example.ZombieClu;
 
 /**
  * Cluster execution profile. Workflow is treated as Job in Parallel Java 2 Environment.
@@ -29,10 +41,22 @@ import edu.rit.pj2.Tuple;
 
 public class ClusterExecutionProfile extends Job implements ExecutionProfile {
 
-	private List<Module> modules;
+	protected Workflow workflow;
+	private String tracker;
+	
+	public ClusterExecutionProfile(){
+		super();
+		tracker = "localhost:20618";
+		init();
+	}
 	
 	public ClusterExecutionProfile(String tracker) {
 		super();
+		this.tracker = tracker;
+		init();
+	}
+	
+	private void init(){
 		try {
 
 			String hp[] = tracker.split(":");
@@ -45,10 +69,18 @@ public class ClusterExecutionProfile extends Job implements ExecutionProfile {
 			throw new ClusterConfigurationException("Incorrect job tracker configuration " + tracker);
 		}
 	}
+	
 
+	public void setTracker(String trackerHostPort) {
+		this.tracker = trackerHostPort;
+		init();
+		
+	}
+	
 	public void setJar(String jarFile){
 		try {
 			jar(new File(jarFile));
+			
 		} catch (IOException e) {
 			throw new ClusterConfigurationException("Failed to load jar file for cluster execution");
 		}
@@ -57,9 +89,16 @@ public class ClusterExecutionProfile extends Job implements ExecutionProfile {
 	@Override
 	public void executeModules(List<Module> modules, List<Reporter> reporters) {
 		try {
-			this.modules = modules;
+			this.workflow = modules.iterator().next().workflow();
+			Global.log().info("Start executing modules, setting up workflow rules");
+	    	
 			main(new String[0]);
+			Global.log().info("Start executing workflows ");
+	    	
 			execute();
+			Global.log().info("Finished executing workflows");
+	    	
+	
 		} catch (Exception e) {
 			new ClusterExecutionProfile("Failed to execute module");
 		}
@@ -67,46 +106,103 @@ public class ClusterExecutionProfile extends Job implements ExecutionProfile {
 
 	
 	/**
-	 * Main rules setup for the jobs.
+	 * Main rules setup for the jobs. This is the master process, which would run at the same place as the job submitter, 
+	 * most probably on front nodes, or the same place where tracker is running.
 	 */
 	@Override
 	public void main(String[] jobArgs) throws Exception {
-		for(Module module : modules){
-			if(module.ready()){
-				module.instantiate();
-				for (ModuleInstance mInstance : module.instances()){
-						putTuple(new ModuleTuple(mInstance));
-						rule().task(ModuleTask.class).args(new String[]{mInstance.module().name()});
-				}
-			}
-		}
+		
+		//Start rule, immediately start executing this instance remotely
+		// How do I pass all the modules to the WorkflowTask ?
+		putTuple(new WorkflowTuple(workflow));
+		rule().task(WorkflowTask.class).args(new String[0]);
+		rule().whenMatch(new ModuleInstanceTuple()).task(ModuleTask.class);
+		
 	}
 
+	private static class WorkflowTask extends Task {
+	
+		/**
+		 * Accept number of initial module involves, not instances. All are available as tuples.
+		 */
+		@Override
+		public void main(String[] args) throws Exception {
+			WorkflowTuple wTuple = takeTuple(new WorkflowTuple());
+			List<Module> modules = wTuple.workflow().modules();
+
+			for(Module module : modules){
+				if(module.ready()){
+					Global.log().info("Instantiating " + module.name());
+					module.instantiate();
+					
+					for (ModuleInstance mInstance : module.instances()){
+							putTuple(new ModuleInstanceTuple(mInstance));
+					}
+					
+					// Gathering remote instances. Blocking. Requiring enough workers.
+					List<ModuleInstance> remoteInstances = new ArrayList<ModuleInstance>();
+					
+					System.out.println("Blocking, and gathering result of execution ");
+					for(int i=0;i<module.instances().size();i++){
+						ExecutedModuleTuple executed = takeTuple(new ExecutedModuleTuple());
+						remoteInstances.add(executed.instance());
+					}
+					
+					module.instances(remoteInstances);
+					System.gc();
+				} 
+			}
+			Global.log().info("All workflow task done");
+		}
+		
+	}
+	
 	/*
-	 * Maybe I already have to take module from tuple here.
+	 * Module task that will be executed remotely.
 	 */
 	private static class ModuleTask extends Task {
 
 		@Override
 		public void main(String[] taskArgs) throws Exception {
-				System.out.println("Running a task, producing tuples " + Arrays.toString(taskArgs));
-				ModuleTuple m = takeTuple(new ModuleTuple());
+				ModuleInstanceTuple m = (ModuleInstanceTuple)getMatchingTuple(0);
 				System.out.println("Executing " + m.instance().module().name());
 				boolean result = m.execute();
 				System.out.println("Result "+ result + " "+m.instance().module().name()+ "  "+m.instance().outputs().iterator().next());
+				putTuple(new ExecutedModuleTuple(m.instance()));
 		}
 		
 	}
 	
-	private static class ModuleTuple extends Tuple {
+	private static class WorkflowTuple extends Tuple {
+		Workflow workflow ;
+		public WorkflowTuple(){
+			
+		}
+		public Workflow workflow(){
+			return workflow;
+		}
+		public WorkflowTuple(Workflow m){
+			workflow = m;
+		}
+		@Override
+		public void readIn(InStream in) throws IOException {
+			workflow = (Workflow) in.readObject();
+		}
+		@Override
+		public void writeOut(OutStream out) throws IOException {
+			out.writeObject(workflow);
+		}
+	}
+	
+	private static class ModuleInstanceTuple extends Tuple {
 		ModuleInstance moduleInstance;
-		public ModuleTuple(){
+		public ModuleInstanceTuple(){
 			
 		}
 		public ModuleInstance instance(){
 			return moduleInstance;
-		}
-		public ModuleTuple(ModuleInstance mi){
+		}  
+		public ModuleInstanceTuple(ModuleInstance mi){
 			moduleInstance = mi;
 		}
 		@Override
@@ -122,4 +218,27 @@ public class ClusterExecutionProfile extends Job implements ExecutionProfile {
 			return moduleInstance.execute();
 		}
 	}
+	
+	private static class ExecutedModuleTuple extends Tuple{
+		ModuleInstance executedInstance;
+		public ExecutedModuleTuple(){
+			
+		}
+		public ExecutedModuleTuple(ModuleInstance mi){
+			executedInstance = mi;
+		}
+		public ModuleInstance instance(){
+			return executedInstance;
+		}
+		@Override
+		public void readIn(InStream in) throws IOException {
+			executedInstance = (ModuleInstance) in.readObject();
+		}
+		@Override
+		public void writeOut(OutStream out) throws IOException {
+			out.writeObject(executedInstance);
+		}
+		
+	}
+
 }
