3ecf833966dd415ad8010ddf6f353f08488a493e
Gerben <Gerben@145.18.167.133>
2015-08-26 11:22:19 +0200

    ...

--- mvn-install-pj2.sh
+++ mvn-install-pj2.sh
@@ -1,7 +1,7 @@
 #I included PJ2 Library from http://www.cs.rit.edu/~ark/pj2.shtml in the lib directory
 #Since it's nowhere provided in any maven repository, the following command would install it in your local repository so you can continue building ducktape.
 #It is referred to in ducktape maven pom.xml
-
+# Windows users in Eclipse can make a new Maven run configuration based on the following command
 
 mvn install:install-file -Dfile=lib/pj2.jar -DgroupId=edu.rit.pj2 -DartifactId=parallel-java -Dversion=1.0 -Dpackaging=jar
 
